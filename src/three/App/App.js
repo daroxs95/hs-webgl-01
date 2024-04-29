18317a82f0036d696110513ed524b666d66b398a
Darián López <daroxs95@gmail.com>
2024-04-29 19:54:31 +0200

    Render cube with stats

--- src/three/App/App.js
+++ src/three/App/App.js
@@ -1,10 +1,14 @@
-import {WebGLRenderer} from "three";
+import {PerspectiveCamera, Scene, WebGLRenderer} from "three";
+import Stats from "stats.js";
 
 export default class App {
     _gl
+    _camera
+    _scene
+    _stats
 
     constructor() {
-        console.log('App constructor');
+        this.init();
     }
 
     init() {
@@ -13,5 +17,45 @@ export default class App {
         });
 
         this._gl.setSize(window.innerWidth, window.innerHeight);
+        const aspectRatio = window.innerWidth / window.innerHeight;
+        this._camera = new PerspectiveCamera(75, aspectRatio, 0.1, 1000);
+
+        this._scene = new Scene();
+
+        this._stats = new Stats();
+        document.body.appendChild(this._stats.dom);
+
+        this.handleEvents();
+    }
+
+    render() {
+        this._stats.begin();
+        this._gl.render(this._scene, this._camera);
+        this._stats.end();
+        window.requestAnimationFrame(() => this.render());
+    }
+
+    resize() {
+        this._gl.setSize(window.innerWidth, window.innerHeight);
+        this._camera.aspect = window.innerWidth / window.innerHeight;
+        this._camera.updateProjectionMatrix();
+    }
+
+    handleEvents() {
+        window.addEventListener('resize', () => {
+            this.resize();
+        });
+    }
+
+    getScene() {
+        return this._scene;
+    }
+
+    getCamera() {
+        return this._camera;
+    }
+
+    getGlConteext() {
+        return this._gl;
     }
 }
--- src/three/index.js
+++ src/three/index.js
@@ -1,3 +1,17 @@
 import App from "./App/App";
+import {BoxGeometry, Mesh, MeshBasicMaterial} from "three";
+import {OrbitControls} from "three/addons";
 
-new App().init();
+const app = new App();
+
+app.getScene().add(new Mesh(
+    new BoxGeometry(1, 1, 1),
+    new MeshBasicMaterial({color: 0xff0000})
+));
+
+app.getCamera().position.z = 5;
+
+new OrbitControls(app.getCamera(), app.getGlConteext().domElement);
+
+app.render();
+
