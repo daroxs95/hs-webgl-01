14395616349765981d38fc8d623804be8d824544
Darián López <daroxs95@gmail.com>
2024-05-07 17:06:11 +0200

    Add animations utilities
    - Add `prepModelAndAnimations`

---
+++ src/three/App/Resources/Animations.js
@@ -0,0 +1,7 @@
+export function prepModelAndAnimations(model) {
+    const animsByName = {};
+    model.animations?.forEach((clip) => {
+        animsByName[clip.name] = clip;
+    });
+    model.animsByName = animsByName;
+}
