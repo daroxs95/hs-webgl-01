1aacdee71ee25d78d8d9bd7606924fea354b3479
Darián López <daroxs95@gmail.com>
2024-05-07 17:09:13 +0200

    Update assets

--- src/three/App/assets.js
+++ src/three/App/assets.js
@@ -1,9 +1,11 @@
 export const resources = [
-    {key: 'tv', type: 'gltf', url: 'assets/tv.glb'},
-    {key: 'screen', type: 'gltf', url: 'assets/screen.glb'},
-    {key: 'knight', type: 'gltf', url: 'assets/mallow_knight/scene.gltf'},
     {key: 'astronaut', type: 'gltf', url: 'assets/cute_astronaut.glb'},
-    {key: 'space', type: 'texture', url: 'assets/space.jpg'},
+    {key: 'astronaut_anim', type: 'gltf', url: 'assets/astronaut_anim.glb'},
     {key: 'planet', type: 'gltf', url: 'assets/planet.glb'},
     {key: 'rocket', type: 'gltf', url: 'assets/rocket.glb'},
+    // {key: 'tv', type: 'gltf', url: 'assets/tv.glb'},
+    // {key: 'screen', type: 'gltf', url: 'assets/screen.glb'},
+    // {key: 'knight', type: 'gltf', url: 'assets/mallow_knight/scene.gltf'},
+    // {key: 'space', type: 'texture', url: 'assets/space.jpg'},
+    // {key: 'mars', type: 'rgbe', url: 'assets/mars.hdr'},
 ]
